7d114959ffa9c3ebe73f8309b43338ef402b9ad4
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-05-04 18:06:32 +0530

    changes in routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import HeaderComp from "./components/Header"
 import Home from "./components/Home/Home"
 import NotFound from "./components/NotFound/NotFound"
